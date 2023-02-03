4edf6d3b476a5c6c7f59eda3bcc34ebcb8f3af8f
Ak_shay k <77868304+Akshayanil-k@users.noreply.github.com>
2023-02-04 02:32:03 +0530

    commit

---
+++ Angular/Lifecycle-hook/src/app/app.component.ts
@@ -0,0 +1,64 @@
+
+
+import {
+  Component, OnInit, AfterContentChecked, AfterContentInit, AfterViewChecked,
+  AfterViewInit, DoCheck, OnChanges, OnDestroy, Input, SimpleChanges
+} from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  displayChild: boolean = false;
+  value: string = 'succes';
+
+  constructor() {
+    console.log("AppComponent:Constructor");
+  }
+
+  toggle() {
+    this.displayChild = !this.displayChild;
+  }
+
+  ngOnChanges() {
+    console.log("AppComponent:OnChanges");
+  }
+
+  ngOnInit() {
+    console.log("AppComponent:OnInit");
+  }
+
+  ngDoCheck() {
+    console.log("AppComponent:DoCheck");
+  }
+  ngAfterContentInit() {
+    console.log("AppComponent:AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AppComponent:AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("AppComponent:AfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AppComponent:AfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("AppComponent:OnDestroy");
+  }
+  onShowOrHide(): void{
+    this.displayChild = !this.displayChild;
+  }
+}
